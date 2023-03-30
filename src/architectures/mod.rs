b67adaaf0901a8b66c8fff57b1606156acf2e18b
Karan Singh Lobana <karansinghlobana@protonmail.com>
2023-03-30 19:48:04 +0530

    structured architectures directory

---
+++ src/architectures/mod.rs
@@ -0,0 +1,16 @@
+pub mod architecture;
+pub mod registers;
+pub mod stacks;
+pub mod instructions;
+pub mod labels;
+pub mod directives;
+pub mod interrupts;
+pub mod programs;
+
+pub use registers::*;
+pub use stacks::*;
+pub use instructions::*;
+pub use labels::*;
+pub use directives::*;
+pub use interrupts::*;
+pub use programs::*;
--- src/lib.rs
+++ src/lib.rs
@@ -1,3 +1,9 @@
 
+#[allow(dead_code)]
+pub mod architectures;
+pub mod machines;
+
 #[cfg(test)]
-mod tests {}
+mod tests {
+
+}
